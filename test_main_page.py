a4e8a82ca6c8b12ec1451ec45dd8d204f7d9247d
OlgaOFrolova <50freesmart@gmail.com>
2023-01-21 15:44:57 +0300

    сделаны проверки формы регистрации

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,27 @@
-from selenium.webdriver.common.by import By
+from pages.login_page import LoginPage
+from pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_should_be_login_page(browser):
+
+     link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+     page1 = LoginPage(browser, link)
+     page1.open()
+     # проверки на наличие форм
+     page1.should_be_login_form()
+     page1.should_be_register_form()
+
+
